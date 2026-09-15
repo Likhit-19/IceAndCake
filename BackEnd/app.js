041822afdab7f93387621754f27277e265b6819a
Likhit-19 <likhitmeshram03@gmail.com>
2026-09-16 01:21:00 +0530

    Resolve Error

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -19,8 +19,8 @@ connnectDB()
     console.log("Something went wrong", error);
 });
 app.use(cors({
-     origin: process.env.FRONTEND_URL||"http://localhost:5173",
-    credentials:true,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
 }));
 connectCloudinary();
 
